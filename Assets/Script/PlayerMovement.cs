196205ae72b13c72869968df3b18353b164f550c
Huwen281127 <125277571+Huwen281127@users.noreply.github.com>
2024-05-02 13:24:53 +0800

    第一人稱射擊
    1.膠囊體
    2.攝影機(第一人稱視角)
    3.前後左右移動(X、Z軸)
    4.減速機制
    5.跳要機制(空白建)

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -2,52 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerCam : MonoBehaviour
 {
-    [Header("移動設定")]
-    public float moveSpeed;
+    [Header("鏡頭轉動敏感度")]
+    public float sensX;   // 鏡頭X軸轉動敏感度
+    public float sensY;   // 鏡頭Y軸轉動敏感度
 
-    [Header("按鍵綁定")]
-    public KeyCode jumpKey = KeyCode.Space;
-
-    [Header("基本設定")]
-    public Transform PlayerCamera;   // 攝影機
-
-    private float horizontalInput;   // 左右方向按鍵的數值(-1 <= X <= +1)
-    private float verticalInput;     // 上下方向按鍵的數值(-1 <= Y <= +1)
-
-    private Vector3 moveDirection;   // 移動方向
-
-    private Rigidbody rbFirstPerson; // 第一人稱物件(膠囊體)的剛體(空的)
+    float xRotation;
+    float yRotaiton;
 
     private void Start()
     {
-        rbFirstPerson = GetComponent<Rigidbody>();
-        rbFirstPerson.freezeRotation = true;         // 鎖定第一人稱物件剛體旋轉，不讓膠囊體因為碰到物件就亂轉
+        Cursor.lockState = CursorLockMode.Locked;   // 鎖定滑鼠游標在畫面中央
+        Cursor.visible = false;                     // 隱藏滑鼠游標
     }
 
     private void Update()
     {
-        MyInput();
-    }
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;   // 取得滑鼠游標的X軸移動
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;   // 取得滑鼠游標的Y軸移動
 
-    private void FixedUpdate()   //跟鋼體有關的東西盡量放這裡面 (移動速度會比較穩定，比放在Update穩定)
-    {
-        MovePlayer(); // 只要是物件移動，建議你放到FixedUpdate()
-    }
+        // 因為預設的XY軸移動方向在UNITY是反轉的，我們要將滑鼠X軸轉成物件的Y軸，Y軸轉成X軸
+        xRotation -= mouseY; // 將滑鼠Y軸移動數值"倒轉"過來(正變負負變正)
+        yRotaiton += mouseX;
 
-    // 方法：取得目前玩家按方向鍵上下左右的數值
-    private void MyInput()                                 //   右   左   上   下
-    {
-        horizontalInput = Input.GetAxisRaw("Horizontal");  //   1   -1    0    0
-        verticalInput = Input.GetAxisRaw("Vertical");     //    0    0    1   -1
-    }
+        xRotation = Mathf.Clamp(xRotation, -90f, 30f); // 限定X軸轉動在正30度到負90度間(抬頭和低頭有限制角度)
 
-    private void MovePlayer()
-    {
-        // 計算移動方向(其實就是計算X軸與Z軸兩個方向的力量)
-        moveDirection = PlayerCamera.forward * verticalInput + PlayerCamera.right * horizontalInput;
-        // 推動第一人稱物件
-        rbFirstPerson.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        transform.rotation = Quaternion.Euler(xRotation, yRotaiton, 0); // 設定攝影機角度
     }
 }
